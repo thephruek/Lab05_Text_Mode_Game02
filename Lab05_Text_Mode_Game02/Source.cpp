62e40eac44da3fac777a012f4cfb5a25bd79e0ab
PHRUEK APIROMRAT <63010650@KMITL.AC.TH>
2020-09-14 17:35:26 +0700

    ammo

--- Lab05_Text_Mode_Game02/Source.cpp
+++ Lab05_Text_Mode_Game02/Source.cpp
@@ -6,20 +6,31 @@ void gotoxy(int x, int y)
 	COORD c = { x, y };
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
 }
+void setcolor(int fg, int bg)
+{
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsole, bg * 16 + fg);
+}
 void draw_ship(int x, int y)
 {
 	gotoxy(x, y); 
-	printf(" <-0-> ");
+	printf("<--0-->");
+}
+void draw_ammo(int x, int y)
+{
+	gotoxy(x += 4, --y);
+	setcolor(14 , 0);
+	printf("A");
 }
 void erase_ship(int x, int y)
 {
 	gotoxy(x, y);
 	printf("       ");
 }
-void setcolor(int fg, int bg)
+void erase_ammo(int x, int y)
 {
-	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-	SetConsoleTextAttribute(hConsole, bg * 16 + fg);
+	gotoxy(x += 4, --y);
+	printf(" ");
 }
 void setcursor(bool visible)
 {
@@ -33,28 +44,53 @@ int main()
 {
 	setcursor(0);
 	char ch = '.';
-	int x = 38, y = 20;
-	draw_ship(x, y);
+	int x1 = 38, y1 = 20, x2 = 38, y2 = 20, i = 0, j = 0;
+	setcolor(2, 4);
+	draw_ship(x1, y1);
+	setcolor(2, 0);
 	do {
 		if (_kbhit()) {
 			ch = _getch();
 			if (ch == 'a') {
-				erase_ship(x, y);
-				setcolor(2, 4);
-				draw_ship(--x, y);
-				setcolor(2, 0);
-				
+				i = 1;
+			}
+			if (ch == 'd') {
+				i = 2;
 			}
 			if (ch == 's') {
-				erase_ship(x, y);
-				setcolor(2, 4);
-				draw_ship(++x, y);
-				setcolor(2, 0);
-				
+				i = 0;
+			}
+			if (ch == ' ') {
+				j = 1;
 			}
 			fflush(stdin);
 		}
-		Sleep(100);
+		if (i == 1 && x1>1) {
+			erase_ship(x1, y1);
+			setcolor(2, 4);
+			draw_ship(--x1, y1);
+			setcolor(2, 0);
+			Sleep(100);
+		}
+		if (i == 2 && x1<113) {
+			erase_ship(x1, y1);
+			setcolor(2, 4);
+			draw_ship(++x1, y1);
+			setcolor(2, 0);
+			Sleep(100);
+		}
+		if (j == 0) {
+			x2 = x1;
+		}
+		if (j == 1 && y2 > 1) {
+			erase_ammo(x2, y2);
+			draw_ammo(x2, --y2);
+		}
+		if (y2 == 1) {
+			erase_ammo(x2, y2);
+			j = 0; 
+			y2 = y2;
+		}
 	} while (ch != 'x');
 	return 0;
 }
